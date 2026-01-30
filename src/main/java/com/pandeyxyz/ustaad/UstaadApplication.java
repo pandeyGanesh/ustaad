9cf4ad49d0e94c449bbc2e607d097ee0bf3e05fb
pandeyGanesh <pandey.ganesh12@gmail.com>
2026-01-31 00:23:51 +0530

    US-1 Repository setup

---
+++ src/main/java/com/pandeyxyz/ustaad/UstaadApplication.java
@@ -0,0 +1,13 @@
+package com.pandeyxyz.ustaad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UstaadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UstaadApplication.class, args);
+	}
+
+}
